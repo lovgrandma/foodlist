afc005541e144b8a7588370afe53100919777aff
lovgrandma <32661318+lovgrandma@users.noreply.github.com>
2020-02-16 00:01:22 -0500

    Basic functionality of foodgroup enums functionality. Foodgroups works as an array for the food class as foods can belong to many foodgroups

--- src/foodbase/FoodGroup.java
+++ src/foodbase/FoodGroup.java
@@ -1,9 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package foodbase;
 
+/**
+ *
+ * @author Owner
+ */
 public enum FoodGroup {
-	Diary,
-	Grains,
-	Protein,
-	Vegetables,
-	Fruits
+    DAIRY(1, "dairy"),
+    GRAINS(2, "grains"),
+    PROTEIN(3, "protein"),
+    VEGETABLES(4, "vegetables"),
+    FRUITS(5, "fruits");
+    
+    private int dispNum;
+    private String dispName;
+    
+    FoodGroup(int dispNum, String dispName) {
+        this.dispName = dispName;
+        this.dispNum = dispNum;
+    }
+    
+    public String getDispName() {
+        return this.dispName;
+    }
+    
+    public void setDispName(String dispName) {
+        this.dispName = dispName;
+    }
+    
+    public String toString() {
+        return this.dispName;
+    }
 }
