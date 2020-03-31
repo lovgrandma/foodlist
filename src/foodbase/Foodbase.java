a2094bbdd73d2fc22c2244a664674b509a04f0ff
lovgrandma <32661318+lovgrandma@users.noreply.github.com>
2020-03-31 14:53:51 -0400

    basic fxml and controller

--- src/foodbase/Foodbase.java
+++ src/foodbase/Foodbase.java
@@ -4,29 +4,38 @@
  * and open the template in the editor.
  */
 package foodbase;
-import java.util.ArrayList;
-import java.util.Scanner;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Jesse
  */
-public class Foodbase {
-   
-    // Small updatae
-    // Simple output method
-    public static void print(String b) {
-        System.out.println(b);
-    }   
+public class Foodbase extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        stage.setTitle("Foodbase!");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
     public static void main(String[] args) {
+        launch(args);
         // This array intializing functionality could go into GUI class. This array is
         // used to add foodgroup values to new food.
-        ArrayList<FoodGroup> foodgroups = new ArrayList();
-        foodgroups.add(FoodGroup.DAIRY);
-        foodgroups.add(FoodGroup.GRAINS);
-        Food a = new Food("Chicken", "A great dish!", foodgroups);
-        print(a.toString());
+//        ArrayList<FoodGroup> foodgroups = new ArrayList();
+//        foodgroups.add(FoodGroup.DAIRY);
+//        foodgroups.add(FoodGroup.GRAINS);
+//        Food a = new Food("Chicken", "A great dish!", foodgroups);
+//        print(a.toString());
     }
         
 }
