da24a2fae3156207ffa9bd0225e911544ce89193
lovgrandma <32661318+lovgrandma@users.noreply.github.com>
2020-04-01 11:35:37 -0400

    add page

--- src/foodbase/Controller.java
+++ src/foodbase/Controller.java
@@ -2,13 +2,19 @@ package foodbase;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ComboBox;
-import javafx.stage.Stage;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -22,15 +28,45 @@ public class Controller implements Initializable {
     private TextField search;
     
     @FXML
-    private Button addNew;
+    private Button addNew, confirmAdd;
     
     @FXML
     private ComboBox filter;
     
+    @FXML
+    void handleAdd(MouseEvent event) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Add.fxml"));
+            Parent root2 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("add food");
+            stage.setScene(new Scene(root2));
+            stage.show();
+            handleAddConfirm();
+            
+        } catch (Exception e) {
+            System.out.println(e + "2");
+        }
+    }
     
+    void handleAddConfirm() throws Exception {
+        confirmAdd.setOnMouseClicked((MouseEvent event2)-> {
+            try {
+                System.out.println("Confirm add");
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        });
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        addNew.setOnMouseClicked((MouseEvent event)-> {    
+            try {
+                this.handleAdd(event);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        });
     }
 }
